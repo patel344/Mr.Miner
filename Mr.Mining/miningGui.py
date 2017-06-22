57d33a05a4e13f7b3fd80b95f0918bb8d3fc344a
patel344 <Parth@Parths-MacBook-Pro.local>
2017-06-22 18:54:29 -0500

    adding preliminary file

---
+++ Mr.Mining/miningGui.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+from subprocess import call
+"""
+
+setx GPU_FORCE_64BIT_PTR 0
+setx GPU_MAX_HEAP_SIZE 100
+setx GPU_USE_SYNC_OBJECTS 1
+setx GPU_SINGLE_ALLOC_PERCENT 100
+setx GPU_MAX_ALLOC_PERCENT 100
+ethminer.exe -F http://eth-eu1.nanopool.org:8888/YOUR_ADDRESS/MINER_NAME/YOUR_EMAIL -U
+
+"""
+
+def ethereumConfig():
+    address = '0x234567890'
+    miner_name = 'parth'
+    email = 'parth@gmail.com'
+    call("setx GPU_FORCE_64BIT_PTR 0", shell=True)
+    call("setx GPU_MAX_HEAP_SIZE 100", shell=True)
+    call("setx GPU_USE_SYNC_OBJECTS 1", shell=True)
+    call("setx GPU_SINGLE_ALLOC_PERCENT 100", shell=True)
+    call("setx GPU_MAX_ALLOC_PERCENT 100", shell=True)
+    call("echo ethminer.exe -F http://eth-eu1.nanopool.org:8888/"+address+"/"+miner_name+"/"+email+" -U", shell=True)
+
+
+
+if __name__ == '__main__':
+    ethereumConfig()
