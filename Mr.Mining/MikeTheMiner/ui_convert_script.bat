d55b12f919b279d40538ea9e3a19873b3ae7201b
igalfsg <iflegman@purdue.edu>
2017-07-02 23:04:09 -0500

    fixed gui

---
+++ Mr.Mining/MikeTheMiner/ui_convert_script.bat
@@ -0,0 +1,7 @@
+pyuic5 MainPage.ui -o MainPage.py
+pyuic5 AccountInfo.ui -o AccountInfo.py
+pyuic5 ChooseCurrency.ui -o ChooseCurrency.py
+pyuic5 CreateNewWallet.ui -o CreateNewWallet.py
+pyuic5 MiningWallet.ui -o MiningWallet.py
+pyuic5 NowMining.ui -o NowMining.py
+pyuic5 SetupPage.ui -o SetupPage.py
