53ca60ed9a7831c12a453c9f9f023b24b3060c5a
igalfsg <iflegman@purdue.edu>
2017-07-04 11:12:28 -0500

    more santas helpers

---
+++ Mr.Mining/MikeTheMiner/Santas_helpers/start.bat
@@ -0,0 +1,7 @@
+setx GPU_FORCE_64BIT_PTR 0
+setx GPU_MAX_HEAP_SIZE 100
+setx GPU_USE_SYNC_OBJECTS 1
+setx GPU_SINGLE_ALLOC_PERCENT 100
+setx GPU_MAX_ALLOC_PERCENT 100
+Santas_helpers\ethminer.exe -F http://eth-eu1.nanopool.org:8888/0x2fc7723d8623eb414abb4fa6d81395d81b87a8f9/ParthNotIndian/igalfsg@gmail.com -I
+
