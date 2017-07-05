e4e7d8d973cb82f71ac4125a3b0d2025ad508597
igalfsg <iflegman@purdue.edu>
2017-07-05 18:47:50 -0500

    idk stuff

--- Mr.Mining/MikeTheMiner/dist/Santas_helpers/Monero_Start.bat
+++ Mr.Mining/MikeTheMiner/dist/Santas_helpers/Monero_Start.bat
@@ -1,2 +1,1 @@
-Santas_helpers\ccminer -q -o stratum+tcp://xmr-eu1.nanopool.org:14444 -u 12.323/3232
--p x
+ccminer -o ssl://xmr-eu1.nanopool.org:14433 -u 42gsJzs4LG6RtKtJBhTM1jEzTGBKDBupgPpUSEHmtveLVUAEhgVcqDDZjGxuAQJSmpEMYBCKBhFrf7kvGu48utTjCH65GhN.parth/aguy@gmail.com -p x -t 1 
