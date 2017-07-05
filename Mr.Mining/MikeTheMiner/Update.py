cb4946bdbab7e46c336b3ef6c40b6fd59c324189
igalfsg <iflegman@purdue.edu>
2017-07-04 19:30:14 -0500

    changed link on update.py

--- Mr.Mining/MikeTheMiner/Update.py
+++ Mr.Mining/MikeTheMiner/Update.py
@@ -3,20 +3,29 @@ import os
 import sys
 #get the argument shit sorted out
 #check other comments
-version = str(sys.argv)
-with urllib.request.urlopen('http://fitlifeapp.com/MrMiner/test.txt') as response:
+files = [f for f in os.listdir('.') if os.path.isfile(f)]
+for f in files:
+    if'MisterMiner' in f:
+        version = f
+with urllib.request.urlopen('http://misterminers.com/' + version) as response:
    html = response.read()
    stuff = html.decode("utf-8").splitlines()#.decode("utf-8").splitlines()
 #urllib.request.urlretrieve ("http://fitlifeapp.com/MrMiner/pasc-logo.png", "yougood.png")
 erase = 0
+santa = 0
 print (stuff[0].split(' ')[1])
 if stuff[0].split(' ')[1] != version:
     for i in stuff[1:]:
         if erase == 0:
+            if i == 'santa':
+                santa = 1
             if i == 'borrar':
                 erase = 1
             else:
                 urllib.request.urlretrieve(i.split(' ')[0], i.split(' ')[1])
+                if santa == 1:
+                    pat = r"Santa_helpers\ "
+                    os.rename(i.split(' ')[1], pat[:-1] + i.split(' ')[1] )
         else:
             if os.path.isfile(i):
                 #maybe have to add process kill before deleteing the mrrobot
